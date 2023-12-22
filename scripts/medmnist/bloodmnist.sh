e60e631d060db4c258ba312ce5b0aee3017ae28c
Lorenzo Querol <lorenzoquerol@gmail.com>
2023-12-22 22:33:12 +0900

    update

--- scripts/medmnist/bloodmnist.sh
+++ scripts/medmnist/bloodmnist.sh
@@ -12,7 +12,7 @@ accelerate launch train_jempp.py \
     --p_x_weight 1.0 \
     --p_y_x_weight 1.0 \
     --l2_weight 0.0 \
-    --n_steps 10 \
+    --n_steps 20 \
     --in_steps 5 \
     --query_size 1000 \
     --dataset bloodmnist \
--- scripts/medmnist/organcmnist.sh
+++ scripts/medmnist/organcmnist.sh
@@ -11,7 +11,7 @@ accelerate launch train_jempp.py \
     --decay_epochs 50 100 125 \
     --p_x_weight 1.0 \
     --p_y_x_weight 1.0 \
-    --l2_weight 0.0 \
+    --l2_weight 0.001 \
     --n_steps 20 \
     --in_steps 5 \
     --query_size 1000 \
