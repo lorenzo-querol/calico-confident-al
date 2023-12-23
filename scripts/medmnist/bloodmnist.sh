71b2750f0fcff5280c486e3d13fe4b5eddb111ae
Lorenzo Querol <lorenzoquerol@gmail.com>
2023-12-24 00:12:51 +0900

    update

--- scripts/medmnist/bloodmnist.sh
+++ scripts/medmnist/bloodmnist.sh
@@ -7,7 +7,7 @@ accelerate launch train_jempp.py \
     --lr 0.1 \
     --optimizer sgd \
     --norm batch \
-    --n_epochs 150 \
+    --n_epochs 100 \
     --decay_epochs 50 100 125 \
     --p_x_weight 1.0 \
     --p_y_x_weight 1.0 \
--- scripts/medmnist/organcmnist.sh
+++ scripts/medmnist/organcmnist.sh
@@ -4,14 +4,14 @@ export CUDA_VISIBLE_DEVICES="0,1,2,3"
 
 accelerate launch train_jempp.py \
     --model yopo \
-    --lr 0.0001 \
-    --optimizer adam \
-    --norm none \
-    --n_epochs 150 \
+    --lr 0.1 \
+    --optimizer sgd \
+    --norm batch \
+    --n_epochs 100 \
     --decay_epochs 50 100 125 \
     --p_x_weight 1.0 \
     --p_y_x_weight 1.0 \
-    --l2_weight 0.01 \
+    --l2_weight 0.0 \
     --n_steps 20 \
     --in_steps 5 \
     --query_size 1000 \
