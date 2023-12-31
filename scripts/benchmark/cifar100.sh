01038b9afbdfd2f864f1fd61981434202b6e8e63
Lorenzo Querol <lorenzoquerol@gmail.com>
2024-01-01 01:13:05 +0900

    update

--- scripts/benchmark/cifar100.sh
+++ scripts/benchmark/cifar100.sh
@@ -4,9 +4,10 @@ export CUDA_VISIBLE_DEVICES="0,1,2,3"
 
 accelerate launch train_jempp.py \
     --model yopo \
-    --lr 0.1 \
-    --optimizer sgd \
-    --norm batch \
+    --lr 0.0001 \
+    --optimizer adam \
+    --norm none \
+    --n_epochs 150 \
     --decay_epochs 50 100 125 \
     --p_x_weight 1.0 \
     --p_y_x_weight 1.0 \
