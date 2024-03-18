668ef69a44b9b1f19d2fe22804b90c04d1a65d20
Lorenzo Querol <lorenzoquerol@gmail.com>
2024-03-18 11:31:15 +0900

    update

--- scripts/train_baseline.sh
+++ scripts/train_baseline.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 
-DATASET=$1
-EXP_NAME=$2
-export CUDA_VISIBLE_DEVICES=$3
+# Define the datasets
+DATASETS=("bloodmnist" "dermamnist" "organcmnist" "organsmnist" "pneumoniamnist")
 
-# Don't forget to change the query size for benchmark or medmnist
-# For benchmark, use 40000
-# For medmnist, use 4000
+EXP_NAME="baseline-softmax"
+export CUDA_VISIBLE_DEVICES=$1
 
-python train_jempp.py \
-    --model yopo \
-    --lr 0.1 --decay_epochs 60 120 180 --optim sgd --norm batch \
-    --n_epochs 200 --batch_size 128 \
-    --px 0.0 --pyx 1.0 --l2 0.0 \
-    --n_steps 10 --in_steps 5 \
-    --query_size 40000 --sample_method random \
-    --dataset $DATASET --exp_name $4 \
+# Loop through the datasets
+for DATASET in "${DATASETS[@]}"
+do
+    # Don't forget to change the query size for benchmark or medmnist
+    # For benchmark, use 40000
+    # For medmnist, use 4000
+
+    python train_jempp.py \
+        --model yopo \
+        --lr 0.01 --decay_epochs 60 120 160 --optim sgd --norm batch \
+        --n_epochs 200 --batch_size 128 \
+        --px 0.0 --pyx 1.0 --l2 0.0 \
+        --n_steps 10 --in_steps 5 \
+        --query_size 4000 --sample_method random \
+        --dataset $DATASET --exp_name ${EXP_NAME}
+done
