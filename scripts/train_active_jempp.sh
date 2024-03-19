cb3ed33f228b1d03c711d0a625c1fae797f799e2
lorenzo-querol <lorenzoquerol@gmail.com>
2024-03-19 09:31:40 +0900

    update

--- scripts/train_active_jempp.sh
+++ scripts/train_active_jempp.sh
@@ -8,9 +8,9 @@ export CUDA_VISIBLE_DEVICES=$2
 # MedMNIST: query_size 250 
 
 python train_jempp.py \
-    --query_size 250 --lr 0.00001 \
-    --model yopo --norm none \
-    --decay_epochs 25 --optim adam --warmup_iters -1 \
+    --query_size 250 --lr 0.01 \
+    --model yopo --norm batch \
+    --decay_epochs 25 --optim sgd --warmup_iters -1 \
     --n_epochs 50 --batch_size 128 \
     --px 1.0 --pyx 1.0 --l2 0.0 \
     --n_steps 10 --in_steps 5 \
--- scripts/train_equal_jempp.sh
+++ scripts/train_equal_jempp.sh
@@ -10,21 +10,10 @@ export CUDA_VISIBLE_DEVICES=$3
 # organsmnist: 3850, labels_per_class 35
 # pneumoniamnist: 2000, labels_per_class 100
 
-# python train_jempp.py \
-#     --query_size 250 --lr 0.01 \
-#     --model yopo --norm batch \
-#     --decay_epochs 25 --optim sgd --warmup_iters -1 \
-#     --n_epochs 50 --batch_size 128 \
-#     --px 1.0 --pyx 1.0 --l2 0.0 \
-#     --n_steps 10 --in_steps 5 \
-#     --sample_method equal --labels_per_class ${LABELS_PER_CLASS} \
-#     --dataset $DATASET --exp_name 'equal-jempp'
-
-# Run this for PneumoniaMNIST
 python train_jempp.py \
-    --query_size 250 --lr 0.00001 \
-    --model yopo --norm none \
-    --decay_epochs 25 --optim adam --warmup_iters -1 \
+    --query_size 250 --lr 0.01 \
+    --model yopo --norm batch \
+    --decay_epochs 25 --optim sgd --warmup_iters -1 \
     --n_epochs 50 --batch_size 128 \
     --px 1.0 --pyx 1.0 --l2 0.0 \
     --n_steps 10 --in_steps 5 \
